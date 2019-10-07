e46bcf4a409c7daa0757014561711e5280270087
Vatsalya-Ketan <Vatsalya.Ketan@bcg.com>
2019-10-07 16:37:30 +0800

    Hello Ketan

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-	    Hello Alen!
-            Edit <code>src/App.js</code> and save to reload.
+	    Hello Ketan!
+          
           </p>
           <a
             className="App-link"
